105c39b42ec82a0a045dd99d93c6596b329c6ca7
Gustavo Guimarães <gustavoguimaraes18@gmail.com>
2017-10-09 15:35:57 -0300

    easter egg

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -115,10 +115,14 @@
 	});
 
 
-
-
-
-
+	var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+	$(document).keydown(function(e) {
+	  kkeys.push( e.keyCode );
+	  if ( kkeys.toString().indexOf( konami ) >= 0 ){
+	    $(document).unbind('keydown',arguments.callee);
+	    $('.mu-hero-overlay').addClass('konami-code');
+	  }
+	});
 
 })( jQuery );
 
